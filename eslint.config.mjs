bd2b8f5245add661bedd70be5572c68a04154067
Vikas Gautam <vikasgautamug2003@gmail.com>
2025-08-23 21:13:19 +0530

    fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,22 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
+// export default eslintConfig;
+
+
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -11,6 +30,19 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    rules: {
+      // Turn off Next.js img warning
+      "@next/next/no-img-element": "off",
+
+      // Turn off JSX apostrophe error
+      "react/no-unescaped-entities": "off",
+
+      // Optionally disable unused vars in case of imports like SparklesIcon
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
